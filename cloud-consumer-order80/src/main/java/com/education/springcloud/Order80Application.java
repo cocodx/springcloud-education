3c0952976993d4530bfd191cb9dffba79c4fc7e9
刘刚 <2cevening@gmail.com>
2022-01-25 23:51:34 +0800

    改造payment和consumer集成eureka client客户端

--- cloud-consumer-order80/src/main/java/com/education/springcloud/Order80Application.java
+++ cloud-consumer-order80/src/main/java/com/education/springcloud/Order80Application.java
@@ -3,6 +3,7 @@ package com.education.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author 正能量导师
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @description 启动类
  * @date 24/1/2022 上午7:25 openFeign
  */
+@EnableEurekaClient//声明当前微服务是客户端
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Order80Application {
 

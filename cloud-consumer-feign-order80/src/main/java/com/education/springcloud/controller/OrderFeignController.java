d790a42f79a0ae6562851b4b2722b111d15b819f
刘刚 <2cevening@gmail.com>
2022-02-06 21:53:58 +0800

    open feign还是不行啊

--- cloud-consumer-feign-order80/src/main/java/com/education/springcloud/controller/OrderFeignController.java
+++ cloud-consumer-feign-order80/src/main/java/com/education/springcloud/controller/OrderFeignController.java
@@ -9,20 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-/**
- * @author 正能量导师
- * @version 1.0
- * @description
- * @date 6/2/2022 下午8:00
- */
 @RestController
-public class PaymentFeignController {
+public class OrderFeignController {
 
     @Resource
     private PaymentFeignService paymentFeignService;
 
-    @GetMapping(value = "consumer/payment/get/{id}")
+    @GetMapping(value = "/consumer/payment/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
         return paymentFeignService.getPaymentById(id);
     }
+
+    @GetMapping(value = "/consumer/payment/feign/timeout")
+    public String paymentFeignTimeout(){
+        return paymentFeignService.paymentFeignTimeout();
+    }
+
+
 }
--- cloud-consumer-feign-order80/src/main/java/com/education/springcloud/service/PaymentFeignService.java
+++ cloud-consumer-feign-order80/src/main/java/com/education/springcloud/service/PaymentFeignService.java
@@ -6,24 +6,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-/**
- * @author 正能量导师
- * @version 1.0
- * @description 远程服务调用的接口
- * @date 6/2/2022 下午7:54
- */
 @Component
-@FeignClient("cloud-payment-service")//指定远程服务调用名称 cloud-payment-service CLOUD-PAYMENT-SERVICE
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")//指定远程调用微服务名称
 public interface PaymentFeignService {
-
-
     @GetMapping(value = "/payment/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
 
-
-//    @PostMapping(value = "/payment/create")
-//    public CommonResult<Payment> createPayment(@RequestBody Payment payment);
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout();
 }
